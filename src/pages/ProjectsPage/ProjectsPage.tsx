c58e855c302c28aa01aeaeb9ab34cbf1776b5cfd
supersloy <supersloy@yandex.ru>
2024-09-10 12:36:43 +0300

    Add new section on landing

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,4 +1,4 @@
-import { Group, Space, Stack, Text } from "@mantine/core";
+import { Button, Group, Paper, Space, Stack, Text } from "@mantine/core";
 import { IconChevronDown } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
 
@@ -15,7 +15,7 @@ export function ProjectsPage() {
     <>
       <Stack
         align="center"
-        gap="sm"
+        gap="32"
         style={{ minWidth: "500px" }}
         mih={"calc(100dvh - 64px"}
       >
@@ -25,29 +25,61 @@ export function ProjectsPage() {
             {t("propoganda")}
           </div>
         </Stack>
-        <div className={styles.ProjectsContainer}>
-          {projects.map((project) => (
-            <ProjectCard key={project.name} {...project} />
-          ))}
-        </div>
-        <div style={{ marginTop: "140px" }}>
-          <Group
-            wrap="nowrap"
-            gap="0.25rem"
-            onClick={() => scrollToTarget("datasets-description")}
-            style={{ cursor: "pointer" }}
-          >
-            <Text size="25px" className={styles.Text} span>
-              Подробнее
-            </Text>
-            <IconChevronDown />
-          </Group>
-        </div>
+        <Group wrap="nowrap" px="56" align="stretch">
+          <Stack w="50%" justify="space-between" gap="lg">
+            <Paper
+              withBorder
+              p="lg"
+              px="xl"
+              flex={1}
+              className={styles.IDHDescription}
+            >
+              <Text lh={1.5} span>
+                What is Lorem Ipsum? <br />
+                <br /> Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry&apos;s
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but also
+                the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum
+              </Text>
+            </Paper>
+            <Button
+              className={styles.StartButton}
+              variant="default"
+              radius="md"
+            >
+              Пройти стартовый курс
+            </Button>
+          </Stack>
+
+          <div className={styles.ProjectsContainer}>
+            {projects.map((project) => (
+              <ProjectCard key={project.name} {...project} />
+            ))}
+          </div>
+        </Group>
+        <Group
+          wrap="nowrap"
+          gap="0.25rem"
+          onClick={() => scrollToTarget("datasets-description")}
+          style={{ cursor: "pointer" }}
+          py="2rem"
+        >
+          <Text size="25px" className={styles.Text} span>
+            Подробнее
+          </Text>
+          <IconChevronDown />
+        </Group>
       </Stack>
       <ProjectFullDescriptions />
       <Space h={40} />
-      <span color="#A020F080" className={styles.Ellipse1}></span>
-      <span color="#A020F080" className={styles.Ellipse2}></span>
+      <span className={styles.Ellipse1}></span>
+      <span className={styles.Ellipse2}></span>
     </>
   );
 }
